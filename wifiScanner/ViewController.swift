2e5f4f627cb52c7a5567ddacb0e6a7eafe786a48
krishna <kushwahakrishna03@gmail.com>
2021-05-21 15:01:58 +0530

    Initial Commit

---
+++ wifiScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wifiScanner
+//
+//  Created by Krishna Kushwaha on 21/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
